7c98c2a18d9623a9503366e40b6eda6718a650c3
haros <haldiraros@gmail.com>
2012-11-10 21:45:23 +0100

    Fixed reading input file (did n't work in some cases because of my lame solution); added checking if the instance of the problem is even solveable

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <math.h>
+
 struct customerlist{
 	int i; 		//number
 	int x,y; 	//coordinates
@@ -40,29 +42,35 @@ int main(int argc, char** argv) {
 		printf("Usage: %s input_file output_file\n",argv[0] );
 		return 1;
 	}
-
 	int Q=0; //vehicle capacity
 	int x_0,y_0,e_0,l_0; //data for depot
 	int i=0,x=0,y=0,q=0,e=0,l=0,d=0; //data of current imput i-number; x,y-coordinates; q-demand; e-ready; l-due date; d-service time;
 	struct customerlist *head=NULL; //pointers
 
+	double cur_cost=0, total_cost=0; //current cost/length of track, combined costs of all tracks
+	int veh_count=0; // number of vehicles/tracks
+	double road=0;
 	/** reading input file **/
 	FILE *input_file=NULL;
 	char buf[1024];
 	input_file=fopen(argv[1],"r");
 	if(input_file==NULL){perror("Error opening input file "); return 1;}
-	while((fgets(buf,1024,input_file)!=NULL) && (strcmp(buf,"NUMBER     CAPACITY\n")!=0) ){ //reaching the needed part in file (probably this is lame )
-		printf("line %s \n",buf); 
-	}
-	fscanf(input_file,"%*d %d",&Q); //whole while above can be done with fscanf in simmilar fasion to the one down
+
+	fscanf(input_file,"%*s VEHICLE NUMBER CAPACITY %*d %d",&Q); //whole while above can be done with fscanf in simmilar fasion to the one down
 	printf("Q %d\n",Q);
 
 	fscanf(input_file," CUSTOMER\nCUST NO. XCOORD. YCOORD. DEMAND READY TIME DUE DATE SERVICE TIME 0 %d %d %*d %d %d %*d\n",&x_0,&y_0,&e_0,&l_0);
 	printf("read for depot : x %d, y %d,  servStart %d, servEND %d\n",x_0,y_0,e_0,l_0);
-	while(!feof(input_file)){
+	while(!feof(input_file) && veh_count!=-1){
 		fscanf(input_file," %d %d %d %d %d %d %d\n",&i,&x,&y,&q,&e,&l,&d);
 		printf("read current : i %d, x %d, y %d, DEMAND %d,  servStart %d, servEND %d, servTIME %d\n",i,x,y,q,e,l,d);
+		road=sqrt((double)((x_0-x)*(x_0-x)+(y_0-y)*(y_0-y)));
+		if( (q>Q) || (road>l) || ( (road > e ? road : e) + d + road) > l_0 ){
+			printf("niedopuszczalne\n");
+			veh_count=-1;
+		}
 		Push(&head,i,x,y,q,e,l,d);
+
 	}
 	fclose(input_file);
 	printf("CVRPTW - done\n");
