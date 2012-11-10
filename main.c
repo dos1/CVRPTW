ef049fc2e64aa9383dda93e5e76fcde1d48e69e2
haros <haldiraros@gmail.com>
2012-11-10 17:52:25 +0100

    Added passing arguments and opening input file

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-	printf("CVRPTW\n");
+	if(argc<3) {
+		printf("Usage: %s input_file output_file\n",argv[0] );
+		return 1;
+	}
+
+	FILE *input=NULL;
+	input=fopen(argv[1],"r");
+	if(input==NULL){perror("Error opening input file "); return 1;}
+
+	printf("CVRPTW - done\n");
+	fclose(input);
 	return 0;
 }
