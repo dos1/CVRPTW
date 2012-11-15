6099ff07cfbb656493831f629c545efb0da74ea0
haros <haldiraros@gmail.com>
2012-11-16 00:57:04 +0100

    Cleanup of comments in main.c

--- main.c
+++ main.c
@@ -125,34 +125,34 @@ int main(int argc, char** argv) {
 
 	processing = true;
 
-	fscanf(input_file,"%*s VEHICLE NUMBER CAPACITY %*d %d",&Q); //whole while above can be done with fscanf in simmilar fasion to the one down
-	//printf("Q %d\n",Q);
+	fscanf(input_file,"%*s VEHICLE NUMBER CAPACITY %*d %d",&Q); //reading Q 
+	
 
 	fscanf(input_file," CUSTOMER\nCUST NO. XCOORD. YCOORD. DEMAND READY TIME DUE DATE SERVICE TIME 0 %d %d %*d %d %d %*d\n",&x_0,&y_0,&e_0,&l_0);
-	//printf("read for depot : x %d, y %d,  servStart %d, servEND %d\n",x_0,y_0,e_0,l_0);
+	//reading data of depot ^
 
 	while (!feof(input_file) && veh_count != -1) {
-		fscanf(input_file," %d %d %d %d %d %d %d\n",&i,&x,&y,&q,&e,&l,&d);
-		//printf("read current : i %d, x %d, y %d, DEMAND %d,  servStart %d, servEND %d, servTIME %d\n",i,x,y,q,e,l,d);
+		fscanf(input_file," %d %d %d %d %d %d %d\n",&i,&x,&y,&q,&e,&l,&d); //reading data of customers
+		
 		road=sqrt((double)((x_0-x)*(x_0-x)+(y_0-y)*(y_0-y)));
 		if ((q>Q) || (road>l) || ((road > e ? road : e) + d + road) > l_0) {
-			//printf("niedopuszczalne\n");
+			//imposible instance
 			veh_count=-1;
 		}
 		customerlist_push_back(&head,&tail,i,x,y,q,e,l,d,road);
 	}
 	fclose(input_file);
-	//printf("%p \n",head);
+	
 	/* creating a additional table of struct endwindows + sorting it */
-	//int n=i; //number of customers
-	int k=0;
+	
+	int k=0; //iterator for the table
 	struct customerlist *pom=NULL;
 	pom=head;
 	struct endwindows *ew_table=(struct endwindows*)calloc(i,sizeof(struct endwindows));
 	while(pom!=NULL){
 		ew_table[k].l=pom->l;
 		ew_table[k].customer=pom;
-		//printf("lol %p - %p\n",pom,ew_table[k].customer);
+		
 		pom=pom->next;
 		k++;
 	}
@@ -160,29 +160,30 @@ int main(int argc, char** argv) {
 	S_sort(ew_table, i);
 
 
-	//for(k=0;k<i;k++) printf ("%d - %p\n",ew_table[k].l,ew_table[k].customer);
+	
 	
 	/*heuristics algoritm part, generating solution */
-	int prev_x=0,prev_y=0, cur_q=0;
-	double prev_road0=0;
-	double road_next=0;
-	struct sol_list *solution=NULL;
-	struct track *cur_track=NULL;
+	int prev_x=0,prev_y=0, cur_q=0; //coordinates of last visited customer, current capacity of the truck
+	double prev_road0=0; //road to depot from the last visited 
+	double road_next=0;  //road to currently tested customer
+	struct sol_list *solution=NULL; //list holding solutions
+	struct track *cur_track=NULL;   // begining of the track
 	pom=NULL;
-	//int ew_count=0; //iterating thingy for table of endwindows
+	
 	while (head!=NULL && processing && veh_count!=-1) {
-		k=0;
+		k=0;                  //setting the starting values
 		prev_x=x_0;
 		prev_y=y_0;
 		cur_q=Q;
 		cur_cost=(double)e_0;
 		cur_track=NULL;
-		//placeholder <-add new track to list of solutions
+		prev_road0 = 0;
+		
 		veh_count++;
 		while (k<i && cur_q>0 && processing) {
 			while (k<i && (double)ew_table[k].l<cur_cost) k++;
 			if (k<i) {
-				pom=ew_table[k].customer; //printf("%d -- %d",pom,ew_table[k].customer); sleep(200);
+				pom=ew_table[k].customer; 
 				road_next=sqrt((double)((prev_x-pom->x)*(prev_x-pom->x)+(prev_y-pom->y)*(prev_y-pom->y)));
 				if( (pom->q<=cur_q) && (road_next+cur_cost<=(double)(pom->l)) && (( road_next+cur_cost>pom->e ? road_next+cur_cost : pom->e) + pom->d + pom->road0 < l_0)){
 					prev_x=pom->x;
@@ -192,8 +193,7 @@ int main(int argc, char** argv) {
 					if (road_next+cur_cost>(double)pom->e) {
 						cur_cost+= road_next + (double)pom->d;
 					} else cur_cost=(double)(pom->e+pom->d);
-					//cur_cost+=(double)(( road_next+cur_cost>(double)pom->e ? road_next : pom->e) + pom->d);
-					//printf("%d ",pom->i);//<- placeholder[[add to solution]] sol->i=pom->i;
+					
 					track_append(&cur_track,pom->i);
 					if ((pom->prev)!=NULL) {
 						(pom->prev)->next=pom->next;
@@ -208,13 +208,13 @@ int main(int argc, char** argv) {
 			}
 			k++;
 		}
-		//printf("\n");
+		
 		sol_list_append(&solution,cur_track);
-		//until I do the correct solution saving
+		
 		total_cost+=cur_cost+prev_road0-(double)e_0;
 	}
 
-
+	/* complete solution if time limit exceeded */
 	if (!processing && head!=NULL){
 		cur_track=NULL;
 		pom=head;
@@ -269,7 +269,7 @@ int main(int argc, char** argv) {
 	} else {
 		usecs = time_end.tv_usec - time_start.tv_usec;
 	}
-	printf("%s\n",argv[1]);
+	printf("%s\n",argv[1]); //printing useful data
 	printf("Time\t\tRoutes\t\tCost\n");
 	printf("%d.%.6d\t%d\t\t%.5f\n", (int)secs, (int)usecs, veh_count, total_cost);
 	return 0;
